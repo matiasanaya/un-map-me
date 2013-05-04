bc2aaef6dbe245fd5d3e74a8d1a09aeab6297ded
Matias Anaya <matiasanaya@gmail.com>
2013-05-04 13:14:50 -0700

    Rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+UnMapMe::Application.config.secret_token = '00b8c54e8430d7b0c7cee9ba18ecb2195ee0cd0f48977bc1a0f6942eb370f96b063fa3a9a32c4c557b18cccc000b78b05b03e71cca58dd720ca140a15c231986'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+UnMapMe::Application.config.session_store :cookie_store, key: '_un-map-me_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# UnMapMe::Application.config.session_store :active_record_store
